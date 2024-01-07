6779b2d43a13735513313bbcda6c33f428bf93c7
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2024-01-08 00:22:27 +0200

    10. Duck typing - first element of a sequence

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,7 +4,11 @@ Module for task 10.
 '''
 from typing import Sequence, Any, Union, Optional
 
+
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """Return the first element of a given sequence,
+    if it exists. Otherwise, return None.
+"""
     if lst:
         return lst[0]
     else:
