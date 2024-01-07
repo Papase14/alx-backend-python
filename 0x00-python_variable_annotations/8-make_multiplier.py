a69dbe72ccd36a2264d690e3c5f7a58bcf5aeb1a
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2024-01-07 18:50:43 +0200

    0x00. Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Module for task 8.
+'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''Creates a multiplier function.
+    '''
+    return lambda x: x * multiplier
